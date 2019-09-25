b51329857cb7ed36ceaaffd7bc61a0426504c572
lmnnnnnnnn <353302347@qq.com>
2019-09-25 22:06:20 +0800

    菜品盈利帕累托

--- Python/YingLi_PaLeiTuo.py
+++ Python/YingLi_PaLeiTuo.py
@@ -16,8 +16,8 @@ plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 plt.figure()
 data.plot(kind='bar')
 plt.ylabel(u'盈利（元）')
-# p = 1.0*data.cumsum()/data.sum()
-# p.plot(color = 'r', secondary_y = True, style = '-o',linewidth = 2)
-# plt.annotate(format(p[6], '.4%'), xy = (6, p[6]), xytext=(6*0.9, p[6]*0.9), arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2")) #添加注释，即85%处的标记。这里包括了指定箭头样式。
-# plt.ylabel(u'盈利（比例）')
+p = 1.0*data.cumsum()/data.sum()
+p.plot(color = 'r', secondary_y = True, style = '-o',linewidth = 2)
+plt.annotate(format(p[6], '.4%'), xy = (6, p[6]), xytext=(6*0.9, p[6]*0.9), arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2")) #添加注释，即85%处的标记。这里包括了指定箭头样式。
+plt.ylabel(u'盈利（比例）')
 plt.show()
