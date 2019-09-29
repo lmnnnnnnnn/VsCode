c881f08b6e5fbb779c2e2920d1d0627a349a8401
lmnnnnnn <353302347@qq.com>
2019-09-29 21:06:41 +0800

    车种之间的分析

---
+++ Python/other/chezhong.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+@Author: mn.l 
+@Date: 2019-09-29 21:03:44
+@Last Modified by: mn.l 
+@Last Modified time: 2019-09-29 21:03:44 
+'''
+from __future__ import print_function
+import pandas as pd
+
+inputfile = r'/Users/Apology/Documents/车种竞争力_李岳思路.xlsx'
+data = pd.read_excel(inputfile,index_col=u'时间(Time)',sheet_name=2) 
+
+print(data.corr()) # 相关系数矩阵，即给出了任意两列之间的相关系数
+# print(data.corr()[u'']) # 只显示‘百合酱蒸凤爪’与其他菜式的相关系数
+# print('相关系数为：%s' % data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺'])) # 计算'百合酱蒸凤爪'与'翡翠蒸香茜饺'的相关系数
