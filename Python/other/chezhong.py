852c9c68291bc0514eddf6ea8e33f5ca1085e3db
lmnnnnnn <353302347@qq.com>
2019-09-29 22:28:25 +0800

    数据整理

--- Python/other/chezhong.py
+++ Python/other/chezhong.py
@@ -12,10 +12,14 @@ import pandas as pd
 inputfile = r'/Users/Apology/Documents/车种竞争力_李岳思路.xlsx'
 data = pd.read_excel(inputfile,index_col=u'时间(Time)',sheet_name=2)
 
-data = data[(data[u'本品最低市场价平均值(Avg_1)'] > 400) & (data[u'本品最低市场价平均值(Avg_1)'] < 100000)]
-# print(data.corr()) # 相关系数矩阵，即给出了任意两列之间的相关系数
-outfile = data.describe()
-# print(data.sum())
-# print(data.corr()[u'']) # 只显示‘百合酱蒸凤爪’与其他菜式的相关系数
-# print('相关系数为：%s' % data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺'])) # 计算'百合酱蒸凤爪'与'翡翠蒸香茜饺'的相关系数
-outfile.to_excel(r'/Users/Apology/Documents/aaa.xlsx',)
+def clean():
+    data = data[(data[u'本品最低市场价平均值(Avg_1)'] > 400) & (data[u'本品最低市场价平均值(Avg_1)'] < 100000)]
+    # print(data.corr()) # 相关系数矩阵，即给出了任意两列之间的相关系数
+    outfile = data.describe()
+    # print(data.sum())
+    # print(data.corr()[u'']) # 只显示‘百合酱蒸凤爪’与其他菜式的相关系数
+    # print('相关系数为：%s' % data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺'])) # 计算'百合酱蒸凤爪'与'翡翠蒸香茜饺'的相关系数
+    outfile.to_excel(r'/Users/Apology/Documents/aaa.xlsx',)
+
+if __name__ == "__clean__":
+    clean
