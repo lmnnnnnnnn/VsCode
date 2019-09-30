c874e9bc925d30dea44f813587dc320f30ab315e
lmnnnnnnnn <353302347@qq.com>
2019-09-30 15:35:55 +0800

    Matplotlib作图

---
+++ Python/数据分析与挖掘实战/chapter3/HuaTu_Head.py
@@ -0,0 +1,11 @@
+''' 
+@Author: Mengnan Li 
+@Date: 2019-09-30 15:32:58 
+@Last Modified by: Mengnan Li 
+@Last Modified time: 2019-09-30 15:32:58 
+'''
+
+import matplotlib.pyplot as plt #导入做图库
+plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+plt.figure(figsize = (7, 5)) #创建图像区域，指定比例
