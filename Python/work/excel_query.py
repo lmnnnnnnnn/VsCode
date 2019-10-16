3bee7827171c90dd77d156bc3cce6e889adee302
lmnnnnnnnn <353302347@qq.com>
2019-10-16 17:45:02 +0800

    excel查询

---
+++ Python/work/excel_query.py
@@ -0,0 +1,16 @@
+''' 
+@Author: Mengnan Li 
+@Date: 2019-10-16 15:04:57 
+@Last Modified by: Mengnan Li 
+@Last Modified time: 2019-10-16 15:04:57
+查询 
+'''
+
+import pandas as pd
+
+inputfile = r'C:\Users\sas\Desktop\FOUND_TIME.xlsx'
+outputfile = r'C:\Users\sas\Desktop\相关系数.xlsx'
+
+data = pd.DataFrame(pd.read_excel(inputfile))
+outdata = data.query("CARLINE_NM == '名图'")
+outdata.to_excel(outputfile)
