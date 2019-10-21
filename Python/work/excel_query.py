d3e21961a27b4e013b77790ff1ec11a35d653d70
lmnnnnnnnn <353302347@qq.com>
2019-10-21 17:58:24 +0800

    change

--- Python/work/excel_query.py
+++ Python/work/excel_query.py
@@ -8,9 +8,19 @@
 
 import pandas as pd
 
-inputfile = r'C:\Users\sas\Desktop\FOUND_TIME.xlsx'
-outputfile = r'C:\Users\sas\Desktop\相关系数.xlsx'
+def main():
+    inputfile = r'C:\Users\sas\Desktop\FOUND_TIME.xlsx'
+    outputfile = r'C:\Users\sas\Desktop\相关系数.xlsx'
 
-data = pd.DataFrame(pd.read_excel(inputfile))
-outdata = data.query("CARLINE_NM == '名图' & PROVINCE == '河北'")
-outdata.to_excel(outputfile)
+    data = pd.DataFrame(pd.read_excel(inputfile))
+
+    # 查
+    outdata = data.query("CARLINE_NM == '名图' & PROVINCE == '河北'")
+
+    # 改
+    outdata2 = outdata.replace('名图','CF') # 更改表中的名称
+    # outdata2 = outdata.rename(columns = lambda x: x.replace('CARLINE_NM','车种'))  # 更改列名
+    outdata2.to_excel(outputfile)
+
+if __name__ == "__main__":
+    main()
